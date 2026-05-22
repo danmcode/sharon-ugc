4850d2eac59e1793c2874574776ec331e702d492
Daniel Alexander Muelas Rivera <danmcode@gmail.com>
2026-05-22 02:33:40 -0500

    add github pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/sharon-ugc',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
